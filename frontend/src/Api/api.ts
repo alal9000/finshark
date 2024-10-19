6552e60423a036272a102d3bf5fe58b98af2243d
aaron <lucidlear@gmail.com>
2024-10-20 09:08:29 +1100

    18 - Dashboard

--- frontend/src/Api/api.ts
+++ frontend/src/Api/api.ts
@@ -1,24 +1,31 @@
-import axios from 'axios';
-import { CompanySearch } from '../Types/company';
-import { REACT_APP_API_KEY } from './key';
-
-interface SearchResponse {
-  data: CompanySearch[];
-}
+import axios from "axios";
+import { CompanyProfile, CompanySearch } from "../Types/company";
+import { REACT_APP_API_KEY } from "./key";
 
 export const searchCompanies = async (query: string) => {
   try {
-    const data = await axios.get<SearchResponse>(
+    const data = await axios.get<CompanySearch[]>(
       `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${REACT_APP_API_KEY}`
     );
     return data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
-      console.log('error message: ', error.message);
+      console.log("error message: ", error.message);
       return error.message;
     } else {
-      console.log('unexpected error: ', error);
-      return 'An unexpected error occured';
+      console.log("unexpected error: ", error);
+      return "An unexpected error occured";
     }
+  }
+};
+
+export const getCompanyProfile = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyProfile[]>(
+      `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message from API: ", error.message);
   }
 };
