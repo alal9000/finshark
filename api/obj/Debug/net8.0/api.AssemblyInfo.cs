0c215fc783eb480def84d9d73f8c069e3c0905e1
aaron <lucidlear@gmail.com>
2024-12-17 19:30:51 +1100

    42 - 1-To-Many CREATE (Post)

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a71b95baf91f06dab49e6af0764c04556a873d28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4542c1a54f3b114b7cb88d2c37985d7ed029e516")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
