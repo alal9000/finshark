0c5e623084ef1929c9abc3e11a664e6d80e2b3bc
aaron <lucidlear@gmail.com>
2025-01-05 17:11:29 +1100

    59 - Data Seeding

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c147125c31998119f02229502c369e81407f3d4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b295af9106e9b9589ab0fd65b2f48f82107a6131")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
