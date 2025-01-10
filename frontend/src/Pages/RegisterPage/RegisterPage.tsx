5d53cf10536659b9ab6cbfc9a8e2f8d847f01b3b
aaron <lucidlear@gmail.com>
2025-01-10 14:29:32 +1100

    64 - Register

--- frontend/src/Pages/RegisterPage/RegisterPage.tsx
+++ frontend/src/Pages/RegisterPage/RegisterPage.tsx
@@ -6,27 +6,28 @@ import { yupResolver } from '@hookform/resolvers/yup';
 
 type Props = {}
 
-type LoginFormsInputs = {
+type RegisterFormsInputs = {
+  email: string;
   userName: string;
   password: string;
 };
 
 const validation = Yup.object().shape({
+  email: Yup.string().required("Email is required"),
   userName: Yup.string().required("Username is required"),
   password: Yup.string().required("Password is required"),
-});
+})
 
-const LoginPage = (props: Props) => {
-  const { loginUser } = useAuth();
-  const { register, handleSubmit, formState: { errors }} = useForm<LoginFormsInputs>({ resolver: yupResolver(validation)});
+const RegisterPage = (props: Props) => {
+   const { registerUser } = useAuth();
+    const { register, handleSubmit, formState: { errors }} = useForm<RegisterFormsInputs>({ resolver: yupResolver(validation)});
+    
+    const handleLogin = (form: RegisterFormsInputs) => {
+      registerUser(form.email, form.userName, form.password);
   
-  const handleLogin = (form: LoginFormsInputs) => {
-    loginUser(form.userName, form.password);
-
-  };
+    };
 
   return (
-    
     <section className="bg-gray-50 dark:bg-gray-900">
     <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <div className="w-full bg-white rounded-lg shadow dark:border md:mb-20 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -35,6 +36,22 @@ const LoginPage = (props: Props) => {
             Sign in to your account
           </h1>
           <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(handleLogin)}>
+          <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Email
+              </label>
+              <input
+                type="text"
+                id="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Email"
+                { ...register("email")}
+              />
+              {errors.email ? <p className="text-white">{errors.email.message}</p> : ""}
+            </div>
             <div>
               <label
                 htmlFor="username"
@@ -98,4 +115,4 @@ const LoginPage = (props: Props) => {
   )
 }
 
-export default LoginPage
+export default RegisterPage
