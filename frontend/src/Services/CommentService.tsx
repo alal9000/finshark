b9ea7a14f069abf59d8943a85c83ef4ea7205608
aaron <lucidlear@gmail.com>
2025-01-14 12:35:17 +1100

    68 - Comment List

--- frontend/src/Services/CommentService.tsx
+++ frontend/src/Services/CommentService.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CommentPost } from "../Models/Comment";
+import { CommentGet, CommentPost } from "../Models/Comment";
 import { handleError } from "../Helpers/ErrorHandler";
 
 const api = "https://localhost:7173/api/comment/";
@@ -10,6 +10,15 @@ export const commentPostAPI = async (title: string, content: string, symbol: str
     title: title,
     content: content
    })
+   return data;
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+export const commentGetAPI = async (symbol: string) => {
+  try {
+   const data = await axios.get<CommentGet[]>(api + `?Symbol=${symbol}`);
    return data;
   } catch (error) {
     handleError(error);
